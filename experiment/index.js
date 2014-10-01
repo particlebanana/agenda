9834b70dbff5e98020ca152abc04b2317cb5fa43
Mike McNeil <mike@balderdash.co>
2014-10-01 12:59:23 -0500

    Solves callback issue- and also smartens up the priority map thing to try and typecast into a number in case the priority is persisted as a string-wrapped number (as apposed to 'lowest', 'highest', etc.).

--- experiment/index.js
+++ experiment/index.js
@@ -24,29 +24,26 @@ Waterline({}, function (err, wl1orm){
       var model = {};
       model.find = _.bind(_model.find, _model);
       model.destroy = function (criteria, cb) {
-        console.warn('** DESTROY: ', util.inspect(Array.prototype.slice.call(arguments, 0, -1), false, null), '\n');
+        // console.warn('** DESTROY: ', util.inspect(Array.prototype.slice.call(arguments, 0, -1), false, null), '\n');
         return _model.destroy(criteria, function(err, results){
-          console.log('RESULTS:',err,results);
+          // console.log('RESULTS:',err,results);
           return cb(err, results);
         });
       };
       model.create = function (values, cb){
-        console.warn('** CREATE: ', util.inspect(Array.prototype.slice.call(arguments, 0, -1), false, null), '\n');
+        // console.warn('** CREATE: ', util.inspect(Array.prototype.slice.call(arguments, 0, -1), false, null), '\n');
         return _model.create(values, function(err, result){
-          console.log('RESULT:',err,result);
-
-          // Make sure `_id` is there
-          result._id = result._id || result.id;
+          // console.log('RESULT:',err,result);
 
           return cb(err, result);
         });
       };
 
       model.update = function (criteria, values, cb) {
-        console.warn('** UPDATE: ', util.inspect(criteria, false, null), '\n|||||||||\n', util.inspect(values, false, null), '\n');
+        // console.warn('** UPDATE: ', util.inspect(criteria, false, null), '\n|||||||||\n', util.inspect(values, false, null), '\n');
 
         return _model.update(criteria, values).exec(function (err, results) {
-          console.log('RESULTS:',err,results);
+          // console.log('RESULTS:',err,results);
           if (err) return cb(err);
 
           // Make sure `_id` is there
@@ -58,34 +55,6 @@ Waterline({}, function (err, wl1orm){
           return cb(null, results);
         });
       };
-      // model.upsert = function (criteria, values, cb) {
-      //   console.warn('** UPSERT: ', util.inspect(criteria, false, null), '\n|||||||||\n', util.inspect(values, false, null), '\n');
-
-      //   // ::update-or-create::
-      //   _model.find(criteria, function (err, records) {
-      //     if (err) return cb(err);
-
-      //     // update
-      //     if (records.length) {
-      //       model.update(criteria, values, function (err, records) {
-      //         console.log('UPSERT (updated)=> RESULTS:',err,records);
-      //         if (err) return cb(err);
-      //         return cb(null, records);
-      //       });
-      //       return;
-      //     }
-
-      //     // or create
-      //     model.create(values, function (err, newRecord) {
-      //       console.log('UPSERT (created)=> RESULTS:',err,[newRecord]);
-      //       if (err) return cb(err);
-      //       return cb(null, [newRecord]);
-      //     });
-      //     return;
-
-      //   });
-      //   return;
-      // };
       return model;
     })(wl1orm.collections.agendajobs),
 
@@ -93,23 +62,35 @@ Waterline({}, function (err, wl1orm){
   });
 
   agenda.maxConcurrency(3);
-  agenda.defaultConcurrency(2);
+  agenda.defaultConcurrency(1);
 
-  // agenda.define('holla', {concurrency: 1, lockLifetime: 5000}, function(job, done) {
-  //   console.log('!!');
-  //   console.log('job  (%s)', job.attrs.data.someId);
-  //   setTimeout(function (){
-  //     console.log('finished    (%s)', job.attrs.data.someId);
-  //     done();
-  //   }, 150);
-  // });
 
-  agenda.define('job which spawns other jobs', {lockLifetime: 5000}, function(job, done) {
-    console.log('\n!!!!!!!!!!!!','\n@@@@@@@@@@@@@@@@@@@@','\n running "job which spawns other jobs"...\n','@@@@@@@@@@@@@@@@@\n!!!!!!!!!!!!!!!!!!\n');
-    return done();
+  agenda.define('holla', {concurrency: 1, lockLifetime: 5000}, function(job, done) {
+    console.log('job  (%s)', job.attrs.data.someId);
+    setTimeout(function (){
+      console.log('finished    (%s)', job.attrs.data.someId);
+      done();
+    }, 150);
   });
 
-  agenda.every('5 seconds', 'job which spawns other jobs');
+  agenda.define('job which spawns other jobs', {lockLifetime: 5000}, function(job, done) {
+    console.log('running "job which spawns other jobs"...');
+    // console.log('(job type: %s)',job.attrs._id);
+
+    async.each(_.range(1,3), function (i, next) {
+      setTimeout(function (){
+        console.log('(((scheduling NEW%s)))', i);
+        agenda.schedule(new Date((new Date()).getTime()+1000), 'holla', { someId: 'NEW'+i} );
+        next();
+      }, 250*i);
+    }, function afterwards(err){
+      if (err) return done(err);
+      return done();
+    });
+  });
+
+  agenda.every('105 seconds', 'job which spawns other jobs');
+
 
   agenda.start();
   console.log('Starting agenda...');
